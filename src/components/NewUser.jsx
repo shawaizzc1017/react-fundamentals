4c10b6a89810b9edde80d9f2e666a7ae611a92ab
unknown <shawaizzc1017@gmail.com>
2026-02-09 12:26:49 +0500

    new component

---
+++ src/components/NewUser.jsx
@@ -0,0 +1,9 @@
+export const NewUser = () => {
+    return (
+        <div>
+            <h2>User</h2>
+            <p>Age: 42</p>
+            <p>email: someone@email.com</p>
+        </div>
+    )
+}
