de22d2600e2d291de5df5c8257dfda29f20ea3e1
unknown <shawaizzc1017@gmail.com>
2026-02-09 14:29:54 +0500

    new component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {Welcome} from "./components/Welcome.jsx";
 import {Button} from "./components/Button.jsx";
 import {User} from "./components/User.jsx";
 import {NewUser} from "./components/NewUser.jsx";
+import {UserDetails} from "./components/UserDetails.jsx";
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
         <Button />
         <User />
         <NewUser />
+        <UserDetails />
     </>
   )
 }
---
+++ src/components/UserDetails.jsx
@@ -0,0 +1,9 @@
+export const UserDetails = () => {
+    return (
+        <div>
+            <h2>User:</h2>
+            <p>Age: 42</p>
+            <p>Email: someone@email.com</p>
+        </div>
+    )
+}
