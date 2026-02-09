1c1103c9168675b29afa5aca87721569225f369b
shawaizzc1017 <shawaizzc1017@gmail.com>
2026-02-09 14:34:16 +0500

    Create UserDetailss.jsx

---
+++ src/components/UserDetailss.jsx
@@ -0,0 +1,11 @@
+export const UserDetailss = () => {
+
+  return (
+        <div>
+            <h2>User:</h2>
+            <p>Age: 42</p>
+            <p>Email: someone@email.com</p>
+        </div>
+    )
+  
+}
